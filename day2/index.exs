da5e3e754da462f566c61a2962c29af13d78139c
Alexander Classen (Westwerk) <ac@westwerk.ac>
2022-12-02 10:54:51 +0100

    day 2

--- day2/index.exs
+++ day2/index.exs
@@ -3,12 +3,84 @@ input = File.read!('input')
 defmodule Parse do
   def parse(input) do
     input
-    |> String.split("\n")
+    |> String.split("\n", trim: true)
     |> Enum.map(&String.split(&1, " "))
   end
 end
 
 guide = Parse.parse(input)
 
+scoreMap = %{"A" => 1, "X"=> 1 , "B" =>2 ,"Y" => 2, "C" => 3, "Z" => 3}
+# A,X = Rock
+# B,Y = Paper
+# C,Z = Scissors
 
-IO.inspect(guide)
+score = Enum.map(guide, fn(game) ->
+  case game do
+    ["A","X"] ->
+      # even
+      Map.get(scoreMap,"X") + 3
+    ["B","Y"] ->
+      # even
+      Map.get(scoreMap,"Y") + 3
+    ["C","Z"] ->
+      # even
+      Map.get(scoreMap,"Z") + 3
+    ["A","Y"] ->
+      # win
+      6 + Map.get(scoreMap,"Y")
+    ["B","Z"] ->
+      # win
+      6 + Map.get(scoreMap,"Z")
+    ["C","X"] ->
+      # win
+      6 + Map.get(scoreMap,"X")
+    ["A","Z"] ->
+      # lose
+      Map.get(scoreMap,"Z")
+    ["B","X"] ->
+      # lose
+      Map.get(scoreMap,"X")
+    ["C","Y"] ->
+      # lose
+      Map.get(scoreMap,"Y")
+  end
+end)
+
+IO.puts('Score: #{Enum.sum(score)}')
+
+
+score = Enum.map(guide, fn(game) ->
+  case game do
+    ["A","X"] ->
+      # lose
+      Map.get(scoreMap,"Z")
+    ["B","Y"] ->
+      # even
+      Map.get(scoreMap,"Y") + 3
+    ["C","Z"] ->
+      # win
+      Map.get(scoreMap,"X") + 6
+    ["A","Y"] ->
+      # even
+      Map.get(scoreMap,"X") + 3
+    ["B","Z"] ->
+      # win
+      Map.get(scoreMap,"Z") + 6
+    ["C","X"] ->
+      # lose
+      Map.get(scoreMap,"Y")
+    ["A","Z"] ->
+      # win
+      Map.get(scoreMap,"Y") + 6
+    ["B","X"] ->
+      # lose
+      Map.get(scoreMap,"X")
+    ["C","Y"] ->
+      # even
+      Map.get(scoreMap,"Z") + 3
+  end
+end)
+
+IO.inspect(score)
+IO.puts('Score: #{Enum.sum(score)}')
