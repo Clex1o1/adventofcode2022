246c0b7e83987716178452c00ca999a25dab9a9a
Alexander Classen (Westwerk) <ac@westwerk.ac>
2022-12-05 13:24:11 +0100

    day4

---
+++ day4/index.exs
@@ -0,0 +1,57 @@
+input = File.read!('input')
+
+defmodule Parse do
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+  end
+end
+
+input = Parse.parse(input)
+flatInput = Enum.map(input, fn(x) ->
+  String.split(x, ",")
+  |> Enum.map(fn(section)->
+    String.split(section, "-")
+    |> Enum.map(fn(startEnd)->
+      String.to_integer(startEnd)
+    end)
+  end)
+  |> Enum.reduce(fn(section2,section1) ->
+    (Enum.at(section1,0) in Enum.at(section2,0)..Enum.at(section2,1) &&
+    Enum.at(section1,1) in Enum.at(section2,0)..Enum.at(section2,1)) ||
+    (Enum.at(section2,0) in Enum.at(section1,0)..Enum.at(section1,1) &&
+    Enum.at(section2,1) in Enum.at(section1,0)..Enum.at(section1,1))
+  end)
+end)
+
+result = Enum.filter(flatInput, fn(item)->
+  item
+end)
+
+
+IO.puts("Result: #{Enum.count(result)}")
+
+## Part 2
+
+flatInput = Enum.map(input, fn(x) ->
+  String.split(x, ",")
+  |> Enum.map(fn(section)->
+    String.split(section, "-")
+    |> Enum.map(fn(startEnd)->
+      String.to_integer(startEnd)
+    end)
+  end)
+  |> Enum.reduce(fn(section2,section1) ->
+    (Enum.at(section1,0) in Enum.at(section2,0)..Enum.at(section2,1) ||
+    Enum.at(section1,1) in Enum.at(section2,0)..Enum.at(section2,1)) ||
+    (Enum.at(section2,0) in Enum.at(section1,0)..Enum.at(section1,1) ||
+    Enum.at(section2,1) in Enum.at(section1,0)..Enum.at(section1,1))
+  end)
+end)
+
+result = Enum.filter(flatInput, fn(item)->
+  item
+end)
+
+
+IO.puts("Result: #{Enum.count(result)}")
